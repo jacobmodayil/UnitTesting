1a71ec0b6370a9209997054d2114396168c868cd
Jacob Modayil <jmodayil@deloitte.com>
2020-03-11 16:37:10 -0500

    Adding parameterized tests

--- TestNinja/TestNinja.UnitTests/MathTests.cs
+++ TestNinja/TestNinja.UnitTests/MathTests.cs
@@ -26,28 +26,16 @@ namespace TestNinja.UnitTests
             Assert.That(result, Is.EqualTo(3));
         }
 
-        [Test]
-        public void Max_FirstArgumentIsGreater_ReturnTheFirstArgument()
-        {
-            var result = _math.Max(2, 1);
-
-            Assert.That(result, Is.EqualTo(2));
-        }
 
         [Test]
-        public void Max_SecondArgumentIsGreater_ReturnTheSecondArgument()
+        [TestCase(2, 1, 2)]
+        [TestCase(1, 2, 2)]
+        [TestCase(2, 2, 2)]
+        public void Add_WhenCalled_ReturnTheGreaterArgument(int a, int b, int expectedResult)
         {
-            var result = _math.Max(1, 2);
+            var result = _math.Max(a, b);
 
-            Assert.That(result, Is.EqualTo(2));
-        }
-
-        [Test]
-        public void Max_ArgumentsAreEqual_ReturnTheSameArgument()
-        {
-            var result = _math.Max(2, 2);
-
-            Assert.That(result, Is.EqualTo(2));
+            Assert.That(result, Is.EqualTo(expectedResult));
         }
     }
 }
