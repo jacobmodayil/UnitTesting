07e0a43189afeb5d97d70353dc0c9bc547e662b0
Jacob Modayil <jmodayil@deloitte.com>
2020-03-12 11:01:39 -0500

    Added Instance Tests

---
+++ TestNinja/TestNinja.UnitTests/CustomerControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [Parallelizable]
+    [TestFixture]
+    public class CustomerControllerTests
+    {
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFound()
+        {
+            var controller = new CustomerController();
+            var result = controller.GetCustomer(0);
+
+            Assert.That(result, Is.TypeOf<NotFound>());
+
+            //InstanceOf means that the result can be a Not Found object or any of its derivatives.
+            Assert.That(result, Is.InstanceOf<NotFound>());
+        }
+    }
+}
--- TestNinja/TestNinja.UnitTests/MathTests.cs
+++ TestNinja/TestNinja.UnitTests/MathTests.cs
@@ -37,5 +37,27 @@ namespace TestNinja.UnitTests
 
             Assert.That(result, Is.EqualTo(expectedResult));
         }
+
+        [Test]
+        public void GetOddNumbers_LimitIsGreaterThanZero_ReturnOddNumbersUpToLimit()
+        {
+            var result = _math.GetOddNumbers(5);
+
+            //General
+            Assert.That(result, Is.Not.Empty);
+            Assert.That(result.Count(), Is.EqualTo(3));
+
+            //Specific
+            Assert.That(result, Does.Contain(1));
+            Assert.That(result, Does.Contain(3));
+            Assert.That(result, Does.Contain(5));
+
+            //Cleaner approach
+            Assert.That(result, Is.EquivalentTo(new[] { 1, 3, 5 }));
+
+            //Useful
+            Assert.That(result, Is.Ordered);
+            Assert.That(result, Is.Unique);
+        }
     }
 }
