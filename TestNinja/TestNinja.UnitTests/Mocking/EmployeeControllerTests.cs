f929e6beae32453ac96f496d5f2b4c790f14f6fb
Jacob Modayil <jmodayil@deloitte.com>
2020-03-16 22:28:43 -0500

    Added EmployeeController, File Downloader and Install Helper tests

---
+++ TestNinja/TestNinja.UnitTests/Mocking/EmployeeControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class EmployeeControllerTests
+    {
+        private Mock<IEmployeeStorage> _storage;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            _storage = new Mock<IEmployeeStorage>();
+        }
+        
+        [Test]
+        [Category("EmployeeController")]
+        public void DeleteEmployee_WhenCalled_DeleteTheEmployeeFromDb()
+        {
+            var controller = new EmployeeController(_storage.Object);
+
+            controller.DeleteEmployee(1);
+
+            _storage.Verify(s => s.DeleteEmployee(1));
+        }   
+    }
+}
